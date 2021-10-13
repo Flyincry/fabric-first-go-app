ea58c3583c4955a4bc49b30ccffcef99e381b2d3
Amazawa <87291077+Amazawa@users.noreply.github.com>
2021-10-13 22:21:26 +0800

    Add files via upload

--- web/webServer.go
+++ web/webServer.go
@@ -17,18 +17,24 @@ func WebStart(app *controllers.Application) {
 	fs := http.FileServer(http.Dir("web/static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.HandleFunc("/", app.IndexView)
+	// http.HandleFunc("/index", app.IndexView)
 	http.HandleFunc("/index.html", app.IndexView)
-	http.HandleFunc("/setInfo.html", app.SetInfoView)
-	http.HandleFunc("/queryReq", app.QueryInfo)
 	http.HandleFunc("/query", app.QueryInfo)
 	http.HandleFunc("/modifyPage", app.ModifyShow)
 	http.HandleFunc("/modify", app.Modify)
 	http.HandleFunc("/Apply", app.Apply)
 
-	fmt.Println("启动Web服务, 监听端口号: 9001")
+	http.HandleFunc("/", app.Account)
+	http.HandleFunc("/home", app.Account)
+	http.HandleFunc("/home/index", app.Account)
+	http.HandleFunc("/home/login", app.Login)
+	http.HandleFunc("/home/welcome", app.Welcome)
+	http.HandleFunc("/home/logout", app.Logout)
+	// http.HandleFunc("/home/logout", accountcontroller.Logout)
 
-	err := http.ListenAndServe(":9001", nil)
+	fmt.Println("启动Web服务, 监听端口号: 9000")
+
+	err := http.ListenAndServe(":9000", nil)
 	if err != nil {
 		fmt.Println("启动Web服务错误")
 	}
